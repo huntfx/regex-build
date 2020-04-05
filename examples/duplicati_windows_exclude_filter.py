ab55503a548b45e0e0d0e6f571074cc3d9ca6c06
Peter Hunt <peter@huntfx.uk>
2020-04-05 12:13:25 +0100

    Improve Duplicati filter

--- examples/duplicati_windows_exclude_filter.py
+++ examples/duplicati_windows_exclude_filter.py
@@ -2,6 +2,9 @@
 This may be added to in the future.
 """
 
+import os
+import sys
+sys.path.append(os.path.normpath(__file__).rsplit(os.path.sep, 2)[0])
 from regex_build import RegexBuild
 
 
@@ -17,38 +20,38 @@ with RegexBuild('.*') as build:
     with build(r'\\') as paths:
         # Block specific files
         paths(
-            'Thumbs.db', 'mntemp', 'UsrClass.dat', 'output_log.txt',
+            'Thumbs.db', 'UsrClass.dat', 'output_log.txt',
             RegexBuild('hyberfil', 'swapfile')('.sys'),
-            RegexBuild('(?i)', exit='$')(r'ntuser\.dat.*'),
             RegexBuild(r'LocalShaderCache-.*\.upk'),
         )
 
-        # Block files with no extension
+        # Block misc files
         paths('(?i)', exit='$')(
-            'lock', 'temp', 'error', 'dump', 'dmp', 'cache',
+            'lock', 'temp', 'error', 'dump', 'dmp', 'cache', 'autoexec.bat',
             RegexBuild('log')('', r'\..*'), RegexBuild('lock')('', 'file'),
         )
 
         # Block specific folders
         paths(
-            'Windows', 'Microsoft', 'NetHood', 'PrintHood', 'Recent', 'SendTo', 'LocalService', 'NetworkService',
+            'Microsoft', 'NetHood', 'PrintHood', 'Recent', 'SendTo', 'LocalService', 'NetworkService', '__pycache__',
             'System Volume Information', 'RECYCLER', r'\$RECYCLE\.BIN', 'I386', 'MSOCache', 'Temporary Internet Files',
             r'Google\\Chrome\\Safe Browsing', 'site-packages', r'\.duplicacy', r'\.git', 'System Volume Information',
-            'WUDownloadCache', '__pycache__',
+            'WUDownloadCache', 'OneDriveTemp', 'Config.MSI', 'Perflogs', RegexBuild('Windows')('', r'\.old'),
         )(r'\\')
 
         # Block misc folders
         with paths('(?i)', exit=r'\\$') as directories:
             directories(
-                'temp', 'tmp', 'temporary', 'dmp', 'telemetry', 'local storage', '.backup', 'safebrowsing',
+                'temp', 'tmp', 'temporary', 'dmp', 'telemetry', 'local storage', '.backup', 'safebrowsing', 'installer',
                 RegexBuild('', 'elevated')('diagnostics'), RegexBuild('hardware')('', ' ')('survey'),
                 RegexBuild('crash')('', 'es', RegexBuild('', ' ', r'\-')('report', 'log', 'dump')('', 's')),
                 RegexBuild(
-                    RegexBuild('web')('', 'app'), 'shader', 'gpu', 'd2ds','code', 'cef', 'package', 'html', 'installer',
+                    RegexBuild('web')('', 'app'), 'shader', 'gpu', 'd2ds','code', 'cef', 'package',
+                    'html', 'installer', 'data', 'file',
                 )('', ' ', r'\-')('cache')('', r'\-temp'),
-                RegexBuild('cache')('', '2', 's', 'storage', RegexBuild('d')('', 'data', 'extensions', 'thumbnails')),
+                RegexBuild('cache')('', 's', 'storage', RegexBuild('d')('', 'data', 'extensions', 'thumbnails')),
                 RegexBuild('dump', 'minidump', 'error')('', 's'),
-                RegexBuild('log')('', 's', 'files'),
+                RegexBuild('log')('', 's', 'files', 'backups'),
             )
 
         # Documents
@@ -70,32 +73,41 @@ with RegexBuild('.*') as build:
                 )(r'\\')
 
             # Roaming
-            appdata(r'Roaming\\')(
-                'NVIDIA', 'Amazon Cloud Drive', 'Code', 'CrashPlan', 'Jedi', r'Tencent\\TXSSO\\SSOTemp',
-                'uTorrent', 'vstelemetry', 'Github Desktop', 'FAHClient', 'Discord', 'Visual Studio Setup',
-                RegexBuild('NZXT')('', ' CAM'),
-                RegexBuild(r'Adobe\\.*')('CT Font ', 'FontFeature', 'Asset', 'Native')(r'Cache'),
-            )(r'\\')
+            with appdata(r'Roaming\\') as roaming:
+                roaming(r'NvTelemetryContainer\.log.*', r'ntuser\.dat', 'mntemp')
+                roaming(
+                    'NVIDIA', 'Amazon Cloud Drive', 'Code', 'CrashPlan', 'Jedi', r'Tencent\\TXSSO\\SSOTemp',
+                    'uTorrent', 'vstelemetry', 'Github Desktop', 'FAHClient', 'Discord', 'Visual Studio Setup',
+                    RegexBuild('NZXT')('', ' CAM'),
+                    RegexBuild(r'Adobe\\.*')('CT Font ', 'FontFeature', 'Asset', 'Native')(r'Cache'),
+                )(r'\\')
 
         # ProgramData
-        # TODO: This needs improvement
-        paths(r'ProgramData\\')('Microsoft.*', 'NVIDIA*', 'NV_Cache')(r'\\')
+        paths(r'ProgramData\\')(
+            'Microsoft.*', 'NVIDIA.*', 'NV_Cache', 'CrashPlan', 'Battle.net', 'Auslogics', 'Autodesk', 'GFACE',
+            'Blizzard Entertainment', 'boost_interprocess', 'Duplicati', 'Epic', 'FLEXnet', r'Mozilla\\Updates',
+            'Packages', 'Path of Building', 'Razer', r'Ubisoft\\Ubisoft Game Launcher', 'USO.*', 'Windows.*', '.mono',
+            r'Adobe\\SLStore', 'AVAST Software', 'CloudBerryLab', 'EA .*', 'For Honor.*', 'Intel', 'Kaspersky Lab',
+            'LiquidTechnologies', 'Oracle', 'Origin', r'regid\.[0-9]{4}\-[0-9]{2}.com.*', 'RuPlatform', 'Samsung',
+
+
+        )(r'\\')
 
         # LocalLow
         paths(r'LocalLow\\')('Mozilla', r'.*\\Unity', r'Nolla_Games_Noita\\Save00\\world\\')
 
         # Mozilla Firefox
-        # TODO: This needs improvement
         # C:\Users\Peter\AppData\Roaming\Mozilla\Firefox\Profiles\xxxxxxxx.Default\datareporting\
         # C:\Users\Peter\AppData\Roaming\Mozilla\Firefox\Profiles\xxxxxxxx.Default\favicons.sqlite-wal
         # C:\Users\Peter\AppData\Roaming\Mozilla\Firefox\Profiles\xxxxxxxx.Default\startupCache\
         with paths(r'Firefox\\.*\\') as firefox:
-            firefox('favicons', 'webappstore')('.sqlite.*')
-            firefox('storage', 'thumbnails', 'datareporting')(r'\\')
-            firefox('startup', 'jumpList')(r'Cache\\')
+            firefox('favicons', 'webappstore', 'cookies', 'content-prefs', 'formhistory')(r'\.sqlite.*')
+            firefox(
+                'storage', 'thumbnails', 'datareporting', 'cache2',
+                RegexBuild('startup', 'jumpList')('Cache'),
+            )(r'\\')
 
         # Google Chrome (it can be moved from AppData, so just check for "/Chrome/*")
-        # TODO: This needs improvement
         # C:\Users\Peter\AppData\Local\Google\Chrome\User Data\Default\ChromeDWriteFontCache
         # C:\Users\Peter\AppData\Local\Google\Chrome\User Data\Default\blob_storage\
         # C:\Users\Peter\AppData\Local\Google\Chrome\User Data\Default\JumpListIconsMostVisited\
@@ -125,4 +137,3 @@ with RegexBuild('.*') as build:
 
 if __name__ == '__main__':
     print(build)
-    # .*(\.((?i)(temp|tmp|cache|dmp|dump|err|crash|part|log(|\..*)|lock(|file))$|(reapeaks|pyc|updaterId|cprestoretmp))|\\((Thumbs.db|mntemp|UsrClass.dat|output_log.txt|(hyberfil|swapfile).sys|(?i)ntuser\.dat.*$|LocalShaderCache-.*\.upk)|(?i)(lock|temp|error|dump|dmp|cache|log(|\..*)|lock(|file))$|(Windows|Microsoft|NetHood|PrintHood|Recent|SendTo|LocalService|NetworkService|System Volume Information|RECYCLER|\$RECYCLE\.BIN|I386|MSOCache|Temporary Internet Files|Google\\Chrome\\Safe Browsing|site-packages|\.duplicacy|\.git|System Volume Information|WUDownloadCache|__pycache__)\\|(?i)(temp|tmp|temporary|dmp|telemetry|local storage|.backup|safebrowsing|(|elevated)diagnostics|hardware(| )survey|crash(|es|(| |\-)(report|log|dump)(|s))|(web(|app)|shader|gpu|d2ds|code|cef|package|html|installer)(| |\-)cache(|\-temp)|cache(|2|s|storage|d(|data|extensions|thumbnails))|(dump|minidump|error)(|s)|log(|s|files))\\$|Documents\\(3DMark*|3DS Max .*\\SimCache\\|Larian Studios\\.*\\LevelCache\\)\\|AppData\\(Local\\(IconCache.db|(\@nzxtcam-app-updater|Amazon Drive|ConnnectedDevicesPlatform|Downloaded Installations|Duplicati|GoToMeeting|Microsoft|MicrosoftEdge\\SharedCacheContainers|OneDrive|Packages|SquirrelTemp|CrashRpt|Comms|GitHubDesktop|4kdownload.com|Package(s| Cache)|NVIDIA(| Corporation)|acquisition\\(sensitive_data|tabcache)|EpicGamesLauncher|UnrealEngine\\.*\\(DerivedDataCache|Intermediate))\\)|Roaming\\(NVIDIA|Amazon Cloud Drive|Code|CrashPlan|Jedi|Tencent\\TXSSO\\SSOTemp|uTorrent|vstelemetry|Github Desktop|FAHClient|Discord|Visual Studio Setup|NZXT(| CAM)|Adobe\\.*(CT Font |FontFeature|Asset|Native)Cache)\\)|ProgramData\\(Microsoft.*|NVIDIA*|NV_Cache)\\|LocalLow\\(Mozilla|.*\\Unity|Nolla_Games_Noita\\Save00\\world\\)|Firefox\\.*\\((favicons|webappstore).sqlite.*|(storage|thumbnails|datareporting)\\|(startup|jumpList)Cache\\)|Chrome\\.*\\((ChromeDWriteFontCache|Favicons.*|Google Profile.*|Cookies|Cookies-journal)|(AutofillStrikeDatabase|blob_storage|BudgetDatabase|Service Worker|data_reduction_proxy_leveldb|Download Service|Feature Engagement Tracker|File System|GCM Store|IndexedDB|JumpListIcons.*|Platform Notifications|Search Logos|shared_proto_db|Site Characteristics Database| Storage|VideoDecodeStats|Web Applications|Pepper.*)\\)|steamapps\\common\\Skyrim\\Data\\((Dawnguard|Dragonborn|HearthFires)|Skyrim(| - ((Animations|Interface|Meshes|Misc|Sounds|Textures|Shaders)|Voices(|Extra))))\.(bsa|esm)))
