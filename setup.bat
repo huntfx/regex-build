74082389aaa941740d800a3e576b28dd4406b587
Peter Hunt <peter@huntfx.uk>
2021-02-12 10:08:57 +0000

    Remove test pypi link

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 IF EXIST dist (rmdir dist /s /q)
 py setup.py sdist
-py -m twine upload --repository testpypi dist/*
+py -m twine upload dist/*
 rmdir dist /s /q
 rmdir regex_build.egg-info /s /q
 PAUSE
